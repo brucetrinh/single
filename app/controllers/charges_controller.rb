5f5b7cdd01b1e01a5e03ae402aa84c4510bcb653
Bruce Trinh <bruce.trinh@yahoo.com.au>
2015-05-10 19:21:14 +1000

    Added form for payment directly on page

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -2,25 +2,51 @@ class ChargesController < ApplicationController
 	def new
 	end
 
-	def create
-	  # Amount in cents
-	  @amount = 500
+	# def create
+	#   # Amount in cents
+	#   @amount = 499
 
-	  customer = Stripe::Customer.create(
-	    :email => 'example@stripe.com',
-	    :card  => params[:stripeToken]
-	  )
+	#   customer = Stripe::Customer.create(
+	#     :email => params[:stripeEmail],
+	#     :card  => params[:stripeToken]
+	#   )
 
-	  charge = Stripe::Charge.create(
-	    :customer    => customer.id,
-	    :amount      => @amount,
-	    :description => 'Rails Stripe customer',
-	    :currency    => 'usd'
-	  )
+	#   charge = Stripe::Charge.create(
+	#     :customer    => customer.id,
+	#     :amount      => @amount,
+	#     :description => 'Rails Stripe customer',
+	#     :currency    => 'usd'
+	#   )
 
-	rescue Stripe::CardError => e
-	  flash[:error] = e.message
-	  redirect_to charges_path
-	end
+	# rescue Stripe::CardError => e
+	#   flash[:error] = e.message
+	#   redirect_to charges_path
+	# end params[:stripeEmail]
+
+def create
+    # Amount in cents
+    amount = 600
+
+ 
+    # Create the customer in Stripe
+    customer = Stripe::Customer.create(
+      email: params[:email],
+      card: params[:stripeToken]
+    )
+ 
+    # Create the charge using the customer data returned by Stripe API
+    charge = Stripe::Charge.create(
+      customer: customer.id,
+      amount: amount,
+      description: 'Test Description',
+      currency: 'usd'
+    )
+ 
+    # place more code upon successfully creating the charge
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to charges_path
+    flash[:notice] = "Please try again"
+  end
 
 end
