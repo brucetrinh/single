90cd12e0c69cdd22265e99c7c77df343089c08af
Bruce Trinh <bruce.trinh@yahoo.com.au>
2015-05-14 11:45:27 +1000

    Added example information for seeds file for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+growth_hacking = Product.create(
+	title: "Growth Hacking",
+	subtitle: "Crash Course",
+	price: "6.00",
+	sku: "GROHACK1"
+	description: %{Enter text in here},
+	)
